d01046e92247a6a16621aff26804a221623b7c73
Niki <nicole.serrano2405@hotmail.com>
2024-09-22 22:52:37 -0600

    Termine tarea 10

---
+++ HOSPITAL1/src/consultas/Consulta.java
@@ -0,0 +1,48 @@
+package consultas;
+
+import consultorios.Consultorio;
+import medicos.Medico;
+import paciente.Paciente;
+
+public class Consulta {
+    public int id;
+
+    public String fechaHora;
+
+    public Paciente paciente;
+
+    public Medico medico;
+
+    public Consultorio consultorio;
+
+    public Consulta(int id, String fechaHora, Paciente paciente, Medico medico, Consultorio consultorio) {
+        this.id = id;
+        this.fechaHora = fechaHora;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.consultorio = consultorio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public Consultorio getConsultorio() {
+        return consultorio;
+    }
+    public String mostrarConsulta() {
+        return String.format("Fecha: %s, Paciente: %s, Medico: %s, Num.Consultorio: %d", fechaHora, paciente, medico, consultorio);
+    }
+}
---
+++ HOSPITAL1/src/paciente/Paciente.java
@@ -0,0 +1,59 @@
+package paciente;
+
+public class Paciente {
+    public String id;
+    public String nombre;
+    public String apellidos;
+    public String fechaNacimiento;
+    public String tipoSangre;
+    public char sexo;
+    private String telefono;
+
+
+    public Paciente(String id, String nombre, String apellidos, String fechaNacimiento, String tipoSangre, char sexo, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.tipoSangre = tipoSangre;
+        this.sexo = sexo;
+        this.telefono = telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String mostrarDatos(){
+        String datos = String.format(
+                "Id: %d, Nombre: %s, Apellidos: %s, Fecha de Nacimiento: %s, Tipo de Sangre: %s, Sexo: %s, Telefono: %s",
+                id, nombre, apellidos, tipoSangre, sexo, telefono);
+
+        return datos;
+    }
+
+}
